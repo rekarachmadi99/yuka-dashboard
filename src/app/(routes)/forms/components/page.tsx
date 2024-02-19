55852dbcaf875dbbe53b8de5e4d04691e4cc623f
rekarachmadi99 <reka.rachmadi@gmail.com>
2024-02-19 17:23:10 +0700

    Updated : penambahan Breadcrumb

--- src/app/(routes)/forms/components/page.tsx
+++ src/app/(routes)/forms/components/page.tsx
@@ -1,11 +1,16 @@
 "use client"
 import MenuContent from "@/components/menu"
+import { Breadcrumb } from "antd"
 import React from "react"
 
 const Components = () => {
   return (
     <MenuContent>
-      <div></div>
+      <Breadcrumb
+        className="py-2 font-medium text-base"
+        items={[{ title: "Forms" }, { title: "Form Component" }]}
+      />
+      <div>s</div>
     </MenuContent>
   )
 }
